403cb8ac7fde06c43e0457b896e4bc33abdaf3d6
Wanda Alifah <wanda.alifah@students.unnes.ac.id>
2024-02-21 13:32:44 +0700

    feat: create admin layout

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
+    <div class="font-poppins">
+        <nav class="py-8 px-20 flex justify-between bg-blue-dark text-white">
+            <div class="flex font-sora text-2xl"><h1>Gadget</h1><h1 class="text-red-500">Out</h1></div>
+            <div>
+                <p>admin_loggedin</p>
+            </div>
+        </nav>
         <slot></slot>
+        <footer class="bg-blue-dark py-8 px-20 text-white flex justify-center">
+            <p class="font-sora text-xs">Kelompok 3 Superbootcamp Nodejs (2024)</p>
+        </footer>
     </div>
 </template>
