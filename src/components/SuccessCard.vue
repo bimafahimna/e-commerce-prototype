6728efc799c286aed8f12d197666237574cc7218
“Andi <andpermana1998@gmail.com>
2024-02-22 20:36:36 +0700

    feat: add loading, error card and success card component

---
+++ src/components/SuccessCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  message: String,
+  customClass: String
+})
+</script>
+
+<template>
+    <div
+    class="text-green-500 bg-green-200 px-4 py-2 mx-auto text-center rounded font-medium w-4/12"
+    :class="[customClass]"
+    >
+        {{ message }}
+    </div>
+</template>
