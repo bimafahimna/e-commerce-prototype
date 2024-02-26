1d5ec711bd6c36195217f3610e6a03bb885794e7
“Andi <andpermana1998@gmail.com>
2024-02-26 14:34:07 +0700

    feat: delete or edit image for create product

--- src/views/admin/product/AddProduct.vue
+++ src/views/admin/product/AddProduct.vue
@@ -7,11 +7,11 @@
             <h1 class="font-bold text-2xl text-center">Product</h1>
             <div class="flex flex-col gap-8 mt-20 mx-auto bg-gray-light rounded-xl p-8 w-1/2">
               <div>
-                <p class="font-bold">Product name</p>
+                <p class="font-bold">Product name<span class="text-red-600">*</span></p>
                 <input v-model="product.name" type="text" class="w-full border-2 rounded p-2">
               </div>
               <div>
-                <p class="font-bold">Product category</p>
+                <p class="font-bold">Product category<span class="text-red-600">*</span></p>
                 <select v-model="product.category_id" class="w-full border-2 rounded p-2" name="category">
                   <option v-for="category in categoryStore.category" :value="category.category_id" :key="category.category_name">{{ category.name }}</option>
                 </select>
@@ -21,7 +21,7 @@
                 <input v-model="product.stock" type="number" class="w-full border-2 rounded p-2" placeholder="10">
               </div>
               <div>
-                <p class="font-bold">Product price</p>
+                <p class="font-bold">Product price<span class="text-red-600">*</span></p>
                 <input v-model="product.price" type="number" class="w-full border-2 rounded p-2" placeholder="1000000">
               </div>
               <div>
@@ -29,12 +29,16 @@
                 <input v-model="product.discount" type="number" class="w-full border-2 rounded p-2" placeholder="0,1 for 10%">
               </div>
               <div>
-                <p class="font-bold mb-2">Product Image</p>
-                <div v-if="!product.image_url" class="relative mx-auto w-32 h-32 bg-blue-300 hover:bg-blue-400 rounded-lg flex items-center justify-center overflow-hidden">
-                  <input type="file" class="absolute inset-0 opacity-0 cursor-pointer w-full h-full" @input="uploadImage" accept="image/png" />
-                  <v-icon scale="1.5" name="hi-upload" />
+                <p class="font-bold mb-2">Product Image <span class="text-gray-400 text-xs">(max.size 500kb)</span></p>
+                <div v-if="!product.image_url" class="relative mx-auto w-32 h-32 bg-blue-300 rounded-lg flex items-center justify-center overflow-hidden" :class="[!isUploading && 'hover:bg-blue-400']">
+                  <input :disabled="isUploading" type="file" :class="['absolute inset-0 opacity-0 w-full h-full', !isUploading && 'cursor-pointer']" @input="uploadImage" accept="image/png" />
+                  <v-icon v-if="!isUploading" scale="1.5" name="hi-upload" />
+                  <v-icon v-else name="fa-spinner" animation="spin-pulse" />
                 </div>
-                <img v-else  class="relative mx-auto w-32 h-32  rounded-lg flex items-center justify-center overflow-hidden" :src="product.image_url" :alt="product.name">
+                <div v-else class="relative mx-auto w-32 h-32 rounded-lg flex items-center justify-center overflow-hidden">
+                  <img class="absolute w-full h-full" :src="product.image_url" :alt="product.name">
+                  <v-icon @click="deleteImage" name="md-cancel" fill="red" class="absolute top-0 right-0 hover:scale-110 cursor-pointer"/>
+                </div>
               </div>
                 <button @click="handleSubmit" class="bg-yellow-400 text-white w-fit px-4 py-2 rounded mx-auto">
                     <div v-if="props.id">Edit</div>
@@ -68,6 +72,8 @@ const product = ref({
   image_url: '',
   category_id: 0
 })
+
+const isUploading = ref(false)
 
 if (props.id) {
   onMounted(() => {
@@ -113,14 +119,24 @@ onMounted(() => {
 })
 
 const uploadImage = async (e) => {
+  isUploading.value = true
   const file = e.target.files[0]
+
   if (!file) {
     alert('Invalid file')
+    isUploading.value = false
+    return
+  }
+
+  if (file.size > 500000) {
+    alert('File exceed max.size allowed(500kb)')
+    isUploading.value = false
     return
   }
 
   if (!product.value.category_id) {
-    alert('Pick one category pleases')
+    alert('Pick one category please')
+    isUploading.value = false
     return
   }
 
@@ -131,10 +147,15 @@ const uploadImage = async (e) => {
     .from('products')
     .upload('Public' + '/' + selectedCategory.name + '/' + `product_${Date.now()}.png`, file)
   if (error) {
-    console.log(error)
     alert(error)
+    isUploading.value = false
     return
   }
   product.value.image_url = `https://oiuykrvppcybmwgvjeys.supabase.co/storage/v1/object/public/${data.fullPath}`
+  isUploading.value = false
+}
+
+const deleteImage = () => {
+  product.value.image_url = ''
 }
 </script>
